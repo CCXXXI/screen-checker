2627bc8d0889e95dbbb02ff84c2ab65b71efcea0
CCXXXI <ccxxxi@qq.com>
2022-07-18 14:15:34 +0800

    test: find_screen for other colors

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,12 +4,20 @@ from pytest import mark
 from main import find_screen
 
 
-@mark.parametrize("file", ["../resources/white 0.jpg"])
-def test_find_screen(file: str):
+@mark.parametrize(
+    "file, color",
+    (
+        *(("../resources/white 0.jpg", c) for c in (None, *range(3))),
+        ("../resources/blue 0.jpg", 0),
+        ("../resources/green 0.jpg", 1),
+        ("../resources/red 0.jpg", 2),
+    ),
+)
+def test_find_screen(file: str, color: int):
     img = cv2.imread(file)
     assert img is not None
 
-    res = find_screen(img)
+    res = find_screen(img, color)
     assert len(res) == 4
     for point in res:
         assert len(point) == 2
