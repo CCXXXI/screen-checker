2d0b889245e30f60d4cb18c659d9c2ccdfc2a887
CCXXXI <ccxxxi@qq.com>
2022-07-18 10:48:57 +0800

    feat: show contours

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,17 @@
-from matplotlib.pyplot import imshow
 import cv2
+import matplotlib.pyplot as plt
+
+from data_types import Image
 
 
-def show(cv_img):
-    imshow(cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB))
+def show(image: Image, contours=None):
+    """
+    Show image for debugging.
+
+    :param image: The image to show.
+    :param contours: Optional contours.
+    """
+    tmp = image.copy()
+    if contours is not None:
+        cv2.drawContours(tmp, contours, -1, (0, 255, 0), 16)
+    plt.imshow(cv2.cvtColor(tmp, cv2.COLOR_BGR2RGB))
