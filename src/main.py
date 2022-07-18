1d4de32ae534e1d58ef31d53e0c9a258731515a6
CCXXXI <ccxxxi@qq.com>
2022-07-18 13:49:18 +0800

    feat: find_screen for more colors

--- src/main.py
+++ src/main.py
@@ -3,15 +3,21 @@ import imutils
 import numpy.typing as npt
 
 
-def find_screen(photo: npt.NDArray) -> npt.NDArray:
+def find_screen(photo: npt.NDArray, color: int = None) -> npt.NDArray:
     """
     Find the screen in the photo.
 
-    :param photo: A photo of the screen. White screen in dark background is expected.
+    :param photo: A photo of the screen.
+    :param color: The color of the screen. Default is white. 0 for blue, 1 for green, 2 for red.
     :return: Four points of the screen.
     """
+    # BGR to gray
+    if color is None:
+        gray = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = photo[..., color]
+
     # get the contours
-    gray = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
     binary = cv2.threshold(gray, None, 255, cv2.THRESH_OTSU)[1]
     contours = imutils.grab_contours(
         cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
