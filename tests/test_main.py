2d37500e1f88b6ec1065cdcf30c69d0a3b22e8e8
CCXXXI <ccxxxi@qq.com>
2022-07-20 13:49:11 +0800

    test: better images and better tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,43 +1,45 @@
+from itertools import product
+from pathlib import Path
+
 import cv2
+import numpy as np
 from pytest import mark, raises
 
 from main import Color, find_screen, check_screen
 
+PASS_LIMIT = 20
+FAIL_LIMIT = 35
+
+photos = {
+    c: set(Path(f"../resources/{c}/").glob("*"))
+    for c in ("blue", "green", "red", "white", "black")
+}
+
 
 @mark.parametrize(
     "file, color",
     (
-        *(
-            ("../resources/white 0.jpg", c)
-            for c in (Color.WHITE, Color.RED, Color.GREEN, Color.BLUE)
-        ),
-        ("../resources/blue 0.jpg", Color.BLUE),
-        ("../resources/green 0.jpg", Color.GREEN),
-        ("../resources/red 0.jpg", Color.RED),
+        *product(photos["white"], set(Color) - {Color.BLACK}),
+        *product(photos["blue"], {Color.BLUE}),
+        *product(photos["green"], {Color.GREEN}),
+        *product(photos["red"], {Color.RED}),
     ),
 )
-def test_find_screen(file: str, color: Color):
-    img = cv2.imread(file)
+def test_find_screen(file: Path, color: Color):
+    img = cv2.imread(file.as_posix())
     assert img is not None
 
-    res = find_screen(img, color)
-    assert len(res) == 4
-    for point in res:
-        assert len(point) == 2
-        assert all(point > 0)
+    corners = find_screen(img, color)
+    assert corners.shape == (4, 2)
+    assert np.all(corners > 0)
 
 
 @mark.parametrize(
     "file, color",
-    (
-        ("../resources/white 0.jpg", Color.BLACK),
-        *(("../resources/blue 0.jpg", c) for c in (Color.GREEN, Color.RED)),
-        *(("../resources/green 0.jpg", c) for c in (Color.BLUE, Color.RED)),
-        *(("../resources/red 0.jpg", c) for c in (Color.BLUE, Color.GREEN)),
-    ),
+    product(photos["black"], Color),
 )
-def test_find_screen_failed(file: str, color: Color):
-    img = cv2.imread(file)
+def test_find_screen_error(file: Path, color: Color):
+    img = cv2.imread(file.as_posix())
     assert img is not None
 
     with raises(ValueError):
@@ -47,30 +49,64 @@ def test_find_screen_failed(file: str, color: Color):
 @mark.parametrize(
     "file, color",
     (
-        ("../resources/white 0.jpg", Color.WHITE),
-        ("../resources/blue 0.jpg", Color.BLUE),
-        ("../resources/green 0.jpg", Color.GREEN),
-        ("../resources/red 0.jpg", Color.RED),
+        *product(photos["white"], {Color.WHITE}),
+        *product(photos["blue"], {Color.BLUE}),
+        *product(photos["green"], {Color.GREEN}),
+        *product(photos["red"], {Color.RED}),
     ),
 )
-def test_check_screen_pass(file: str, color: Color):
-    img = cv2.imread(file)
+def test_check_screen_pass(file: Path, color: Color):
+    img = cv2.imread(file.as_posix())
     assert img is not None
 
-    assert check_screen(img, color, find_screen(img, color)) < 30
+    assert check_screen(img, color, find_screen(img, color)) < PASS_LIMIT
+
+
+@mark.parametrize(
+    "black, white",
+    product(photos["black"], {Path("../resources/white/0.png")}),
+)
+def test_check_screen_pass_black(black: Path, white: Path):
+    img_white = cv2.imread(white.as_posix())
+    img_black = cv2.imread(black.as_posix())
+    assert img_white is not None
+    assert img_black is not None
+
+    assert (
+        check_screen(img_black, Color.BLACK, find_screen(img_white, Color.WHITE))
+        < PASS_LIMIT
+    )
 
 
 @mark.parametrize(
     "file, color, wrong_color",
     (
-        ("../resources/white 0.jpg", Color.WHITE, Color.BLACK),
-        ("../resources/blue 0.jpg", Color.BLUE, Color.WHITE),
-        ("../resources/green 0.jpg", Color.GREEN, Color.RED),
-        ("../resources/red 0.jpg", Color.RED, Color.BLUE),
+        *product(photos["white"], {Color.WHITE}, set(Color) - {Color.WHITE}),
+        *product(photos["blue"], {Color.BLUE}, set(Color) - {Color.BLUE}),
+        *product(photos["green"], {Color.GREEN}, set(Color) - {Color.GREEN}),
+        *product(photos["red"], {Color.RED}, set(Color) - {Color.RED}),
     ),
 )
-def test_check_screen_fail(file: str, color: Color, wrong_color: Color):
-    img = cv2.imread(file)
+def test_check_screen_fail(file: Path, color: Color, wrong_color: Color):
+    img = cv2.imread(file.as_posix())
     assert img is not None
 
-    assert check_screen(img, wrong_color, find_screen(img, color)) > 40
+    assert check_screen(img, wrong_color, find_screen(img, color)) > FAIL_LIMIT
+
+
+@mark.parametrize(
+    "black, white, wrong_color",
+    product(
+        photos["black"], {Path("../resources/white/0.png")}, set(Color) - {Color.BLACK}
+    ),
+)
+def test_check_screen_fail_black(black: Path, white: Path, wrong_color: Color):
+    img_white = cv2.imread(white.as_posix())
+    img_black = cv2.imread(black.as_posix())
+    assert img_white is not None
+    assert img_black is not None
+
+    assert (
+        check_screen(img_black, wrong_color, find_screen(img_white, Color.WHITE))
+        > FAIL_LIMIT
+    )
