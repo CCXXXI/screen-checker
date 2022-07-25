cdce2b2cddf7d235aedba5042776c4f116f9faa9
CCXXXI <ccxxxi@qq.com>
2022-07-25 12:05:07 +0800

    feat: np_type param for cvt_single_color

--- src/opencv_utils.py
+++ src/opencv_utils.py
@@ -2,12 +2,13 @@ import cv2
 import numpy as np
 
 
-def cvt_single_color(src, code):
+def cvt_single_color(src, code, np_type=np.uint8):
     """
     Single color conversion.
 
     :param src: The source color.
     :param code: Something like cv2.COLOR_BGR2HSV.
+    :param np_type: np.uint8 or np.float32 or something else.
     :return: The converted color.
     """
-    return cv2.cvtColor(np.uint8([[src]]), code)[0][0]
+    return cv2.cvtColor(np_type([[src]]), code)[0][0]
--- tests/test_opencv_utils.py
+++ tests/test_opencv_utils.py
@@ -12,5 +12,16 @@ from opencv_utils import cvt_single_color
         ((0, 128, 255), cv2.COLOR_BGR2HSV, (15, 255, 255)),
     ),
 )
-def test_cvt_single_color(src, code, expected):
+def test_cvt_single_color_uint8(src, code, expected):
     assert np.array_equal(cvt_single_color(src, code), expected)
+
+
+@mark.parametrize(
+    "src, code, expected",
+    (
+        ((0, 0.5, 1), cv2.COLOR_BGR2RGB, (1, 0.5, 0)),
+        ((0, 0.5, 1), cv2.COLOR_BGR2HSV, (30, 1, 1)),
+    ),
+)
+def test_cvt_single_color_float32(src, code, expected):
+    assert np.allclose(cvt_single_color(src, code, np.float32), expected)
